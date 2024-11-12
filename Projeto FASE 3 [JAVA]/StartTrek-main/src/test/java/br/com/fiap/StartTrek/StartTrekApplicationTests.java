d5fe8ef85b926edfad5bf6020644551fbbe52c28
devjuliusotto <juliusottode@gmail.com>
2024-11-12 01:30:55 -0300

    Inserindo nova versão

---
+++ Projeto FASE 3 [JAVA]/StartTrek-main/src/test/java/br/com/fiap/StartTrek/StartTrekApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.StartTrek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartTrekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
